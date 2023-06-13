46396c1d8ed861d0f60170ac1852877f3a32cca9
Vu Viet Duy <vuvietduy1234@gmail.com>
2023-06-13 21:36:17 +0700

    Jun 13, 2023

---
+++ Hello World.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void testCase() {
+	string s;
+	cin>>s;
+	cout<<s;
+}
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--) {
+		testCase();
+		cout<<endl;
+	}
+}
